b7f9583311340891b651c687cba92f4b9ab33983
SachaVarisco <varisco54@gmail.com>
2022-05-31 11:17:33 -0300

    push escenas

---
+++ src/scenes/play.js
@@ -0,0 +1,22 @@
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
+var keyR;
+
+export class play extends Phaser.Scene {
+    constructor(){
+        super("play")
+    }
+
+    preload(){
+        //precargar los tiles
+    }
+    create(){
+        //crear mapa
+    }
+}
